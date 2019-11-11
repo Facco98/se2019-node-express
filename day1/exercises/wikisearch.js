52961fc8859fa03bd91866d6c781555bb4a11a24
Claudio Facchinetti <claudiofacchinetti@MacBook-Pro-di-Claudio.local>
2019-11-11 11:17:32 +0100

    Finiti gli esercizi del day1

--- day1/exercises/wikisearch.js
+++ day1/exercises/wikisearch.js
@@ -1,1 +1,21 @@
 /* Implement your solution here */
+const fetch = require('node-fetch');
+
+const argument = process.argv[2] || 'Albert Einstein';
+const url =  `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${encodeURIComponent(argument)}&format=json`;
+
+let printTop5 = async (searchTerm) => {
+
+  let res = await fetch(url);
+  let json = await res.json();
+  let queryResults = json.query.search;
+
+  for( let i = 0; i < 5 && i < queryResults.length; i++ ){
+
+    console.log(queryResults[i].title);
+
+  }
+
+};
+
+printTop5(argument);
--- day1/exercises/zlatan-server.js
+++ day1/exercises/zlatan-server.js
@@ -1,1 +1,25 @@
 /* Implement your solution here */
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const port = !isNaN(process.argv[2]) ? process.argv[2] : 8080;
+const firstName = 'Zlatan';
+const lastName = 'Ibrahimovic';
+const url = `http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`;
+
+app.get('/', (request, response) => {
+  console.log('Ciao mondo');
+  response.status(200).send('Per utilizzare il servizio visita <a href="./zlatan-joke"> /zlatan-joke </a>');
+
+});
+
+app.get('/zlatan-joke', async (request, response) => {
+
+  let res = await fetch(url);
+  let json = await res.json()
+  const joke = json.value.joke;
+  response.status(200).send(joke);
+
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
